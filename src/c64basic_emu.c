da3cbcfe1703460c3bead593209f0e233eaa926b
ninja <atrex66@gmail.com>
2026-07-10 15:26:15 +0200

    removed nested comments from the emu

--- src/c64basic_emu.c
+++ src/c64basic_emu.c
@@ -13,7 +13,7 @@
 #include "hardware/pwm.h"
 #include "hardware/i2c.h"
 #include "hardware/dma.h"
-#include "tusb.h"        // fontos!
+#include "tusb.h"        
 #include "c64basic_emu.h"
 #include "6502_opcodes.h"
 #include "palette256.h"
@@ -34,7 +34,7 @@ extern uint16_t dma_transfer_count;
 extern uint8_t dma_incr;
 extern uint8_t dma_size;
 
-#define RESET_PIN 23  // GPIO pin for reset button
+#define RESET_PIN 23  
 
 void core1_entry();
 
@@ -47,7 +47,7 @@ static void inject_key(uint8_t petscii) {
     memory[C64_KEY_BUFFER + memory[C64_KEY_COUNT]] = petscii;
     memory[C64_KEY_COUNT]++;
     while(memory[C64_KEY_COUNT] > 4) {
-        sleep_ms(10);  // Wait until there's space in the keyboard buffer
+        sleep_ms(10);  
     }
 }
 
@@ -57,7 +57,7 @@ void print_bin(unsigned int v) {
     }
 }
 
-// ─────────────────────────────────────────────────────────────────────────────
+
 void to_hex8(uint8_t value, char *buffer) {
     const char hex_chars[] = "0123456789ABCDEF";
     buffer[0] = hex_chars[(value >> 4) & 0x0F];
@@ -79,7 +79,7 @@ char cmdbuffer[32] = {0};
 void put_char(char b) {
     static int buffer_index = 0;
     if (!is_buffering && b != '{' && b != 0x1b) {
-        inject_key(ascii_to_petscii(b)); // Inject the character directly if not buffering
+        inject_key(ascii_to_petscii(b)); 
         return;
     } else if ((b == '{' || b == 0x1b) && !is_buffering)
     {
@@ -89,10 +89,10 @@ void put_char(char b) {
     if (is_buffering)
     {
         cmdbuffer[buffer_index++] = b;
-        cmdbuffer[buffer_index+1] = '\0'; // Null-terminate the command string
+        cmdbuffer[buffer_index+1] = '\0'; 
         if (buffer_index + 1 > sizeof(cmdbuffer) - 1) {
-            state.halt_reason = KEYBOARD_BUFFER_OVERFLOW_REASON;  // Set halt reason to indicate buffer overflow
-            memcpy(state.disassembly, cmdbuffer, sizeof(cmdbuffer));  // Copy the command that caused the overflow to disassembly
+            state.halt_reason = KEYBOARD_BUFFER_OVERFLOW_REASON;  
+            memcpy(state.disassembly, cmdbuffer, sizeof(cmdbuffer));  
         }
         for (int i = 0; i < sizeof(terminal_keys) / sizeof(terminal_keys[0]); i++) {
             if (strcmp(cmdbuffer, terminal_keys[i].seq) == 0) {
@@ -105,12 +105,12 @@ void put_char(char b) {
         if (b == '}' || b == '\n' || b == '\r') 
         {
             if (cmdbuffer[1]=='$'){
-                // hexadecimal input, convert to PETSCII and inject
+                
                 uint8_t hex_value = (uint8_t)strtol(&cmdbuffer[2], NULL, 16);
                 inject_key(hex_value);
             }
-            is_buffering = false;  // End buffering on closing brace or newline
-            buffer_index = 0;  // Reset buffer index
+            is_buffering = false;  
+            buffer_index = 0;  
         }
     } 
 }
@@ -125,32 +125,32 @@ void reset_irq(uint gpio, uint32_t events) {
 
 bool timer_callback() {
     memory[0xD012]++;
-    return true;  // Keep the timer repeating
+    return true;  
 }
 
 int main()
 {
 
-    // Raise voltage before raising clock, lower clock before lowering voltage
+    
     vreg_set_voltage(CPU_CORE_VOLTAGE);
-    sleep_ms(50);  // let regulator settle
+    sleep_ms(50);  
     set_sys_clock_khz(CPU_CLOCK_KHZ, true);
-    sleep_ms(50);  // let regulator settle
+    sleep_ms(50);  
     stdio_init_all();
-    sleep_ms(1000);  // wait for USB serial to be ready
+    sleep_ms(1000);  
 
     uint8_t counter = 0;
     uint64_t timing=0;
     int sox = BORDER_SIDE;
     int soy = BORDER_TOP;
-    bool blink = true;  // cursor blink state
-    int increment = 1;  // sprite movement increment
-    uint32_t last_irq_us = time_us_32();  // CIA1 60 Hz IRQ timer
+    bool blink = true;  
+    int increment = 1;  
+    uint32_t last_irq_us = time_us_32();  
     uint8_t last_key_pressed = 0;
-    Buffer *border_buffer = create_buffer(TOTAL_W, TOTAL_H, ' ', '@'); // full frame buffer including border
-    SpriteAsset_t sprites[SPRITE_LEN] = {0};  // Array to hold sprite data
-    memset(sprites, 0, sizeof(sprites));  // Initialize sprite data to zero
-    multicore_launch_core1(core1_entry);  // start 6502 VM on core 1
+    Buffer *border_buffer = create_buffer(TOTAL_W, TOTAL_H, ' ', '@'); 
+    SpriteAsset_t sprites[SPRITE_LEN] = {0};  
+    memset(sprites, 0, sizeof(sprites));  
+    multicore_launch_core1(core1_entry);  
     gpio_set_irq_enabled_with_callback(
         RESET_PIN,
         GPIO_IRQ_EDGE_FALL,
@@ -164,21 +164,21 @@ int main()
     init_terminal(TOTAL_W, TOTAL_H);
     clear_terminal();
     init_big_characters('a', ' ', 31);
-    set_framerate(50); // Set the desired framerate to 50 FPS    
+    set_framerate(50); 
     char life=0;
     set_palette(palette256, sizeof(palette256) / sizeof(palette256[0]));
-    int previous_fifo_index = -1;  // Initialize previous FIFO index for change detection
-    ringbuffer_init();  // Initialize the ring buffer for keyboard input
+    int previous_fifo_index = -1;  
+    ringbuffer_init();  
     while (true) 
     {
         if (state.halt_flag && state.halt_reason != 0) {
-            memset(screen, 0x20, FRAME_WIDTH * FRAME_HEIGHT);  // Clear screen to spaces
-            memset(color_data, 0x01, FRAME_WIDTH * FRAME_HEIGHT);  // Clear
-            memset(background, 0x00, FRAME_WIDTH * FRAME_HEIGHT);  // Clear
+            memset(screen, 0x20, FRAME_WIDTH * FRAME_HEIGHT);  
+            memset(color_data, 0x01, FRAME_WIDTH * FRAME_HEIGHT);  
+            memset(background, 0x00, FRAME_WIDTH * FRAME_HEIGHT);  
             render_frame();
-            printf("\033[2j\033[0m");  // reset attributes to default before printing
-            printf("\033[31m"); // set text color to red
-            printf("\033[0;0H"); // move cursor to top-left corner
+            printf("\033[2j\033[0m");  
+            printf("\033[31m"); 
+            printf("\033[0;0H"); 
             printf("****************************************************\n");
             printf("*                  SOFTWARE ERROR                  *\n");
             printf("*              Press RESET to restart              *\n");
@@ -188,36 +188,36 @@ int main()
             printf(" CPU halted. Check the disassembly for details. *\n");
             printf(" PC:$%04X, SP:$%02X, A:$%02X, X:$%02X, Y:$%02X\n", state.program_counter, state.stack_pointer, state.accumulator, state.x_register, state.y_register);
             printf(" Flags: N=%d V=%d D=%d I=%d Z=%d C=%d\n", state.negative_flag, state.overflow_flag, state.decimal_mode, state.interrupt_disable, state.zero_flag, state.carry_flag);
-            printf("\033[0m");  // reset attributes to default after printing   
+            printf("\033[0m");  
             while (gpio_get(23) == 1) {
                 reset_cpu(&state);
             }
         }
 
-        while (tud_cdc_available() && ringbuffer_available() > 1) {  // Check if there's data available in the USB CDC buffer and space in the ring buffer
+        while (tud_cdc_available() && ringbuffer_available() > 1) {  
             char c = tud_cdc_read_char();
-            ringbuffer_push((uint8_t)c);  // Push the character into the ring buffer
+            ringbuffer_push((uint8_t)c);  
         }
 
         frame_start();
         memcpy(sprites, &memory[SPRITE_START_ADDRESS], sizeof(sprites));
-        state.dirty_sprite = false;  // Reset dirty flag
+        state.dirty_sprite = false;  
         hide_terminal_cursor();
-        fill_background_buffer(border_buffer, memory[BORDER_COLOR_ADDRESS]); // Fill border buffer with default background color
-        copy_to_screen(border_buffer, 0, 0); // Copy border buffer to screen
-        printf_atc(0, 0, 0, "%d", state.mops); // Copy text and color data to border buffer
-        uint8_t cur_x = memory[CURSOR_X_ADDRESS];     // 0..39
-        uint8_t cur_y = memory[CURSOR_Y_ADDRESS];     // 0..24
-        uint8_t cur_enable = memory[CURSOR_ENABLE_ADDRESS];  // 0 = cursor on, otherwise off
+        fill_background_buffer(border_buffer, memory[BORDER_COLOR_ADDRESS]); 
+        copy_to_screen(border_buffer, 0, 0); 
+        printf_atc(0, 0, 0, "%d", state.mops); 
+        uint8_t cur_x = memory[CURSOR_X_ADDRESS];     
+        uint8_t cur_y = memory[CURSOR_Y_ADDRESS];     
+        uint8_t cur_enable = memory[CURSOR_ENABLE_ADDRESS];  
         set_cursor_position(cur_x + sox, cur_y + soy);
         if (cur_enable == 0) {
             if (blink) {
-                show_cursor(); // Show cursor
+                show_cursor(); 
             } else {
-                hide_cursor(); // Hide cursor
+                hide_cursor(); 
             }
         } else {
-            hide_cursor(); // Hide cursor
+            hide_cursor(); 
         }
 
         for (int i = 0; i < 25; i++)
@@ -236,18 +236,18 @@ int main()
     #if use_host
         send_screen_packet(blink);
     #else
-        state.frame_ready_flag = true;  // set the frame ready flag in the CPU state
-        memory[FRAME_READY_ADDRESS] = 1;  // set the frame ready flag in memory
+        state.frame_ready_flag = true;  
+        memory[FRAME_READY_ADDRESS] = 1;  
         render_frame();
-        //printf("\n\033[0m");
-        //printf("OP/MS: %d\033[K", state.mops);
-        //printf("IRQ address: $%04X\033[K\n", memory[0x0314] | (memory[0x0315] << 8));
-        //printf("NMI address: $%04X\033[K\n", memory[0x0318] | (memory[0x0319] << 8));
+        
+        
+        
+        
     #endif
         frame_end();
-        if (time_us_64() - timing > 500000) {  // Toggle blink every 500 ms
+        if (time_us_64() - timing > 500000) {  
             timing = time_us_64();
-            blink = !blink; // Toggle blink state
+            blink = !blink; 
         }
     }
     return 0;
@@ -255,13 +255,13 @@ int main()
 
 void core1_entry() {
 
-    //mutex_init(&memoryMutex);  // Initialize the mutex for memory access
+    
     uint32_t counter = 0;
     uint32_t mops = 0;
 
-    // I2C shadow
+    
     uint8_t i2c_ctrl_old  = 0;
-    uint8_t i2c_speed_old = 0xFF;  // force first-run init
+    uint8_t i2c_speed_old = 0xFF;  
 
     uint32_t time_offset = 0;
     uint32_t last_counter = 0;
@@ -275,18 +275,18 @@ void core1_entry() {
     #define I2C_SIZE (I2C_SPEED_ADDR - I2C_CTRL_ADDR + 1)
     memset(&memory[I2C_CTRL_ADDR], 0, I2C_SIZE);
 
-    uint32_t last_tick_us = time_us_32();  // CIA1 50 Hz IRQ timer
-    uint8_t last_key_count = 10;  // Last key count for detecting changes in the keyboard buffer
-    uint32_t retrace_time_us = 0;  // Time of the last raster line update
-    int previous_fifo_index = -1;  // Initialize previous FIFO index for change detection
+    uint32_t last_tick_us = time_us_32();  
+    uint8_t last_key_count = 10;  
+    uint32_t retrace_time_us = 0;  
+    int previous_fifo_index = -1;  
         
     while (true) {
 
         counter++;
 
-        if (memory[C64_KEY_COUNT] < 2) {  // Check if there's space in the keyboard buffer
+        if (memory[C64_KEY_COUNT] < 2) {  
             char key;
-            if (ringbuffer_pop((uint8_t*)&key)) {  // Pop a character from the ring buffer
+            if (ringbuffer_pop((uint8_t*)&key)) {  
                 put_char(key);
             }
         }
